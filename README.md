fd5b46bfd094596d5f2cd41e1f7a793515d946f8
Pete Warden <pete@petewarden.com>
2022-11-05 21:30:01 -0700

    Implememted play/pause for LG TVs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# person_sensor_tv_remote
+# TV Remote Control with a Person Sensor
+
 Pause and play your TV when you get up from the couch, using a Person Sensor and a Circuit Playground Express
+
+https://circuitpython.org/board/circuitplayground_express/
--- code.py
+++ code.py
@@ -1,39 +1,18 @@
+# Play and pause your TV using a Person Sensor.
 
-import pulseio
 import adafruit_irremote
 from adafruit_circuitplayground.express import cpx
+
+import board
+import busio
+import digitalio
 import time
 import struct
-import displayio
-import digitalio
-import busio
-import board
-import bitmaptools
-while True:
-    if cpx.button_a:
-        print("Button A pressed! \n")
-        cpx.red_led = True
-        # encoder.transmit(pulseout, 0x20DF10EF)
-        # encoder.transmit(pulseout, [0xdf, 0x20, 0xef, 0x10])
-        # 0x20DF0DF2
-        encoder.transmit(pulseout, [0xdf, 0x20, 0xf2, 0x0d])
-        # encoder.transmit(pulseout, [223, 32, 239, 16])
-        cpx.red_led = False
-        # wait so the receiver can get the full message
-        time.sleep(0.2)
-    if cpx.button_b:
-        print("Button B pressed! \n")
-        cpx.red_led = True
-        # 0x20DF5DA2
-        encoder.transmit(pulseout, [0xdf, 0x20, 0xa2, 0x5d])
-        cpx.red_led = False
-        time.sleep(0.2)
+import pulseio
 
-# Example of accessing the Person Sensor from Useful Sensors on a Pico using
-# CircuitPython. See https://usfl.ink/ps_dev for the full developer guide.
-
-
-PAUSE_IR_CODES =
+# See https://gist.github.com/francis2110/8f69843dd57ae07dce80.
+PLAY_IR_CODES = [0xdf, 0x20, 0xf2, 0x0d]
+PAUSE_IR_CODES = [0xdf, 0x20, 0xa2, 0x5d]
 
 # The person sensor has the I2C ID of hex 62, or decimal 98.
 PERSON_SENSOR_I2C_ADDRESS = 0x62
@@ -56,6 +35,14 @@ PERSON_SENSOR_RESULT_BYTE_COUNT = struct.calcsize(PERSON_SENSOR_RESULT_FORMAT)
 # How long to pause between sensor polls.
 PERSON_SENSOR_DELAY = 0.2
 
+# How long to wait to pause and play. Alter these to adjust the behavior.
+PAUSE_DELAY_SECONDS = 5
+PLAY_DELAY_SECONDS = 1
+
+# Convert timeout seconds into loop iteration counts.
+PAUSE_DELAY_COUNT = int(PAUSE_DELAY_SECONDS / PERSON_SENSOR_DELAY)
+PLAY_DELAY_COUNT = int(PLAY_DELAY_SECONDS / PERSON_SENSOR_DELAY)
+
 # The Pico doesn't support board.I2C(), so check before calling it. If it isn't
 # present then we assume we're on a Pico and call an explicit function.
 try:
@@ -74,7 +61,8 @@ encoder = adafruit_irremote.GenericTransmit(header=[9500, 4500], one=[550, 550],
                                             zero=[550, 1700], trail=0)
 
 time_since_face_seen = 0
-is_paused = False
+time_face_seen = 0
+is_playing = True
 
 # Keep looping and reading the person sensor results.
 while True:
@@ -111,10 +99,17 @@ while True:
     has_face = (num_faces > 0)
     if has_face:
         time_since_face_seen = 0
+        time_face_seen += 1
     else:
         time_since_face_seen += 1
-
-    if time_since_main_face_seen == FACE_TIMEOUT_COUNT:
-        print("Locking!")
+        time_face_seen = 0
+    if is_playing is True:
+        if time_since_face_seen == PAUSE_DELAY_COUNT:
+            encoder.transmit(pulseout, PAUSE_IR_CODES)
+            is_playing = False
+    else:
+        if has_face and time_face_seen > PLAY_DELAY_COUNT:
+            encoder.transmit(pulseout, PLAY_IR_CODES)
+            is_playing = True
 
     time.sleep(PERSON_SENSOR_DELAY)
